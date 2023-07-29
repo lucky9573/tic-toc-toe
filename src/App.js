2ede9e98149cf292491d42cdeb815d13ed3b633b
likhith <kumarlikhith675@gmail.com>
2023-07-29 06:28:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+const Square = () => {
+    return (
+        <>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+
+        </>
+    )
+}
+
+
+const App = Square
+
+export default App;
